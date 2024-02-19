0d935f9bad5ac8d16fd11be361487eecda29b237
Emmanuel-Cor-Eus01 <119893044+Emmanuel-Cor-Eus01@users.noreply.github.com>
2024-02-19 13:29:39 -0600

    ci: pedir nombre

--- scripts.js
+++ scripts.js
@@ -1,1 +1,11 @@
 console.log("Hola CH38");
+
+function Mensaje() {
+    alert("Hola usuario")
+}
+
+
+function enviarSaludoAPersona() {
+    let message = prompt("Hola usuario dime tu nombre");
+    alert(`Hola ${message}`);
+}
